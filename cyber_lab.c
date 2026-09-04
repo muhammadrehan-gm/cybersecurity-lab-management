a12b3861d2baa6f1aa451092bd5b4cdde2efe700
Muhammad Rehan <rehan030009@gmail.com>
2026-09-05 00:46:32 +0500

    Lab managment system file uploaded

---
+++ cyber_lab.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    char Lab_name[30];
+    int Number_of_computers;
+    int Number_of_network_devices;
+    int Number_of_security_tools;
+    int cost_per_computer;
+    int cost_per_network_device;
+    int Annual_security_software_cost;
+
+    int Computer_cost;
+    int Network_cost;
+    int Total_lab_investment;
+
+    printf("\nEnter the Lab Name:");
+    scanf("%s", Lab_name);
+
+    printf("\nEnter the Number of computers:");
+    scanf("%d", &Number_of_computers);
+
+    printf("\nEnter the Number of Network devices:");
+    scanf("%d", &Number_of_network_devices);
+
+    printf("\nEnter the Number of Security tools:");
+    scanf("%d", &Number_of_security_tools);
+
+    printf("\nEnter the cost per Computer:");
+    scanf("%d", &cost_per_computer);
+
+    printf("\nEnter the cost per network device:");
+    scanf("%d", &cost_per_network_device);
+
+    printf("\nEnter the Annual security software cost:");
+    scanf("%d", &Annual_security_software_cost);
+
+    Computer_cost = Number_of_computers * cost_per_computer;
+    Network_cost = Number_of_network_devices * cost_per_network_device;
+    Total_lab_investment = Computer_cost + Network_cost + Annual_security_software_cost;
+
+    printf("\n\n\n CYBERSECURITY LAB REPORT\n\n");
+    printf("Lab Name: %s\n", Lab_name);
+    printf("Compters: %d\n", Number_of_computers);
+    printf("Network Devices: %d\n", Number_of_network_devices);
+    printf("Security Tools: %d\n\n", Number_of_security_tools);
+    printf("Computer Cost: %d\n", Computer_cost);
+    printf("Network Device Cost: %d\n", Network_cost);
+    printf("Software cost: %d\n\n\n", Annual_security_software_cost);
+
+    printf("Total Lab Investment: %d\n\n\n", Total_lab_investment);
+
+    return 0;
+}
